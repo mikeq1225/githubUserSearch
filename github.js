ec5fe94cd7d6f916fdbd9ce788af7c909f61d720
Michael Quarne <mikedq1225@gmail.com>
2020-09-02 11:01:48 -0700

    finished project

--- github.js
+++ github.js
@@ -1,22 +1,27 @@
 class Github {
-  constructor() {
-    this.client_id = 'd9308aacf8b204d361fd';
-    this.client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b';
-    this.repos_count = 5;
-    this.repos_sort = 'created: asc';
-  }
+	constructor() {
+		this.client_id = "d9308aacf8b204d361fd"
+		this.client_secret = "84969aeef73956f4ec9e8716d1840532802bb81b"
+		this.reposCount = 10
+		this.reposSort = "created: asc"
+	}
 
-  async getUser(user) {
-    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+	async getUser(user) {
+		const profileResponse = await fetch(
+			`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+		)
 
-    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+		const repoResponse = await fetch(
+			`https://api.github.com/users/${user}/repos?per_page=${this.reposCount}&sort=${this.reposSort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+		)
 
-    const profile = await profileResponse.json();
-    const repos = await repoResponse.json();
+		const profile = await profileResponse.json()
+		const repos = await repoResponse.json()
+		console.log(repos)
 
-    return {
-      profile,
-      repos
-    }
-  }
+		return {
+			profile,
+			repos,
+		}
+	}
 }
